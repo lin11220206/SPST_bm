70fd3ea5983fb9f8efd4def6faa773d3052b3123
lin11220206 <lin11220206@gmail.com>
2018-03-19 03:53:04 -0700

    run.sh use in log_180319

--- run.sh
+++ run.sh
@@ -1,26 +1,79 @@
-./a.out rule_set_100K/acl1_100k > log3/acl1
-echo "a1"
-./a.out rule_set_100K/acl2_100k > log3/acl2
-echo "a2"
-./a.out rule_set_100K/acl3_100k > log3/acl3
-echo "a3"
-./a.out rule_set_100K/acl4_100k > log3/acl4
-echo "a4"
-./a.out rule_set_100K/acl5_100k > log3/acl5
-echo "a5"
-./a.out rule_set_100K/fw1_100k > log3/fw1
-echo "f1"
-./a.out rule_set_100K/fw2_100k > log3/fw2
-echo "f2"
-./a.out rule_set_100K/fw3_100k > log3/fw3
-echo "f3"
-./a.out rule_set_100K/fw4_100k > log3/fw4
-echo "f4"
-./a.out rule_set_100K/fw5_100k > log3/fw5
-echo "f5"
-./a.out rule_set_100K/ipc1_100k > log3/ipc1
-echo "i1"
-./a.out rule_set_100K/ipc2_100k > log3/ipc2
-echo "i2"
+./a.out rule_set_100K/acl1_100k B 0 11 6 > log_180319/ACL1_B_0_11_6
+echo '.'
+./a.out rule_set_100K/acl1_100k C 0 16 6 > log_180319/ACL1_C_0_16_6
+echo '.'
+./a.out rule_set_100K/acl1_100k D 0 16 0 > log_180319/ACL1_D_0_16_0
+echo '.'
 
+./a.out rule_set_100K/acl2_100k B 0 16 10 > log_180319/ACL2_B_0_16_10
+echo '.'
+./a.out rule_set_100K/acl2_100k C 0 16 6 > log_180319/ACL2_C_0_16_6
+echo '.'
+./a.out rule_set_100K/acl2_100k D 0 16 6 > log_180319/ACL2_D_0_16_6
+echo '.'
 
+./a.out rule_set_100K/acl3_100k B 0 16 6 > log_180319/ACL3_B_0_16_6
+echo '.'
+./a.out rule_set_100K/acl3_100k C 0 16 0 > log_180319/ACL3_C_0_16_0
+echo '.'
+./a.out rule_set_100K/acl3_100k D 1 16 14 > log_180319/ACL3_D_1_16_14
+echo '.'
+
+./a.out rule_set_100K/acl4_100k B 0 16 6 > log_180319/ACL4_B_0_16_6
+echo '.'
+./a.out rule_set_100K/acl4_100k C 0 16 0 > log_180319/ACL4_C_0_16_0
+echo '.'
+./a.out rule_set_100K/acl4_100k D 1 16 6 > log_180319/ACL4_D_1_16_6
+echo '.'
+
+./a.out rule_set_100K/acl5_100k D 1 16 6 > log_180319/ACL5_D_1_16_6
+echo '.'
+
+./a.out rule_set_100K/fw1_100k B 0 16 6 > log_180319/FW1_B_0_16_6
+echo '.'
+./a.out rule_set_100K/fw1_100k C 0 16 0 > log_180319/FW1_C_0_16_0
+echo '.'
+./a.out rule_set_100K/fw1_100k D 0 16 0 > log_180319/FW1_D_0_16_0
+echo '.'
+
+./a.out rule_set_100K/fw2_100k B 0 16 14 > log_180319/FW2_B_0_16_14
+echo '.'
+./a.out rule_set_100K/fw2_100k C 0 16 0 > log_180319/FW2_C_0_16_0
+echo '.'
+./a.out rule_set_100K/fw2_100k D 1 16 0 > log_180319/FW2_D_1_16_0
+echo '.'
+
+./a.out rule_set_100K/fw3_100k B 0 16 6 > log_180319/FW3_B_0_16_6
+echo '.'
+./a.out rule_set_100K/fw3_100k C 0 16 0 > log_180319/FW3_C_0_16_0
+echo '.'
+./a.out rule_set_100K/fw3_100k D 0 16 0 > log_180319/FW3_D_0_16_0
+echo '.'
+
+./a.out rule_set_100K/fw4_100k B 0 11 6 > log_180319/FW4_B_0_11_6
+echo '.'
+./a.out rule_set_100K/fw4_100k C 0 16 6 > log_180319/FW4_C_0_16_6
+echo '.'
+./a.out rule_set_100K/fw4_100k D 0 16 0 > log_180319/FW4_D_0_16_0
+echo '.'
+
+./a.out rule_set_100K/fw5_100k B 0 16 6 > log_180319/FW5_B_0_16_6
+echo '.'
+./a.out rule_set_100K/fw5_100k C 0 16 0 > log_180319/FW5_C_0_16_0
+echo '.'
+./a.out rule_set_100K/fw5_100k D 0 16 0 > log_180319/FW5_D_0_16_0
+echo '.'
+
+./a.out rule_set_100K/ipc1_100k B 0 16 14 > log_180319/IPC1_B_0_16_14
+echo '.'
+./a.out rule_set_100K/ipc1_100k C 0 16 14 > log_180319/IPC1_C_0_16_14
+echo '.'
+./a.out rule_set_100K/ipc1_100k D 0 16 13 > log_180319/IPC1_D_0_16_13
+echo '.'
+
+./a.out rule_set_100K/ipc2_100k B 0 16 0 > log_180319/IPC2_B_0_16_0
+echo '.'
+./a.out rule_set_100K/ipc2_100k C 0 16 0 > log_180319/IPC2_C_0_16_0
+echo '.'
+./a.out rule_set_100K/ipc2_100k D 0 16 0 > log_180319/IPC2_D_0_16_0
+echo '.'
