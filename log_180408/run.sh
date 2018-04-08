24caf7aaa23e67e0ace90a92167fdfbc0e7f864d
lin11220206 <lin11220206@gmail.com>
2018-04-08 21:26:31 +0800

    add new result

---
+++ log_180408/run.sh
@@ -0,0 +1,11 @@
+../a.out ../rule_set_100K/acl1_100k setting/ACL1 > ACL1
+echo "."
+
+
+../a.out ../rule_set_100K/acl3_100k setting/ACL3 > ACL3
+echo "."
+
+
+../a.out ../rule_set_100K/ipc1_100k setting/IPC1 > IPC1
+echo "."
+
