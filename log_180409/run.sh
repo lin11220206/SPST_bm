b9af992aaed521a85ff32a20abe708c58d51c348
lin11220206 <lin11220206@gmail.com>
2018-04-09 14:23:53 +0800

    fix result data

---
+++ log_180409/run.sh
@@ -0,0 +1,9 @@
+../a.out ../rule_set_100K/acl2_100k setting/ACL2 > ACL2
+echo "."
+
+../a.out ../rule_set_100K/acl4_100k setting/ACL4 > ACL4
+echo "."
+
+../a.out ../rule_set_100K/acl5_100k setting/ACL5 > ACL5
+echo "."
+
